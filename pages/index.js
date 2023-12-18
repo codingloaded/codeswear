fb323b74ff036b7e521c8a98aad419c9641a74c3
roni <codingloaded@gmail.com>
2023-12-18 09:43:20 +0530

    glbal css changed for tailwind and home css delete

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,8 +14,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <h1 className='text-red-700 bg-black'>This is roni</h1>
+      <main >
+        <h1 className='text-red-700'>This is roni</h1>
       </main>
     </>
   )
