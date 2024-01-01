e01e9ed452e73a1b6303111fc563827f87fdbffe
roni <codingloaded@gmail.com>
2024-01-01 09:42:01 +0530

    t-shirt added and navbar and footer displayed to all page by _app.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,14 +5,14 @@ import { FaCartShopping } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    <div className='flex flex-col md:flex-row justify-center md:justify-start items-center mx-5 space-x-5'>
-      <div className="logo "><Image src="/logo.png" alt="" width = {60} height = {100}/></div>
+    <div className='flex flex-col md:flex-row justify-center md:justify-start items-center px-5 space-x-5 mb-1 shadow-md'>
+      <div className="logo "><Link href = "/"><Image src="/logo.png" alt="" width = {60} height = {100}/></Link></div>
       <div className="nav">
         <ul className='flex space-x-3'>
-          <li><Link href = "/">T-Shirts</Link></li>
-          <li><Link href = "/">Hoodies</Link></li>
-          <li><Link href = "/">Mugs</Link></li>
-          <li><Link href = "/">Stickers</Link></li>
+          <li><Link href = "/tshirts">T-Shirts</Link></li>
+          <li><Link href = "/hoodies">Hoodies</Link></li>
+          <li><Link href = "/mugs">Mugs</Link></li>
+          <li><Link href = "/stickers">Stickers</Link></li>
         </ul>
       </div>
       <div className="cart absolute right-5 top-2 mx-5 "><FaCartShopping className='md:text-xl text-2xl'/></div>
---
+++ pages/stickers.js
@@ -0,0 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
+import React from 'react'
+
+const Stickers = () => {
+  return (
+    <div>
+        Stickers
+    </div>
+  )
+}
+
+export default Stickers
