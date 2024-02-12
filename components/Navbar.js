4f1bdc744036a039d5d088289742f66c36e01be1
roni <codingloaded@gmail.com>
2024-02-12 09:16:44 +0530

    order page created video 44 competed

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,8 +6,9 @@ import { IoMdCloseCircleOutline } from "react-icons/io";
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
 import { IoBagCheckOutline } from "react-icons/io5";
+import { MdAccountCircle } from "react-icons/md";
 
-const Navbar = ({cart, addTocart, removeFromcart, clearcart, subTotal}) => {
+const Navbar = ({ cart, addTocart, removeFromcart, clearcart, subTotal }) => {
   // console.log({cart, addTocart, removeFromcart, clearcart, subTotal})
   const toggleCart = () => {
     if (ref.current.classList.contains('translate-x-full')) {
@@ -31,26 +32,32 @@ const Navbar = ({cart, addTocart, removeFromcart, clearcart, subTotal}) => {
           <li><Link href="/stickers">Stickers</Link></li>
         </ul>
       </div>
-      <div className="cart absolute right-5 top-2 mx-5 cursor-pointer" onClick={toggleCart}><FaCartShopping className='md:text-xl text-2xl' /></div>
+
+      <div className="cart absolute right-5  mx-5 cursor-pointer flex" >
+        <MdAccountCircle className='md:text-xl text-2xl mx-2'/>
+        <FaCartShopping className='md:text-xl text-2xl' onClick={toggleCart} />
+      </div>
       {/* cart starts here  */}
-      <div ref={ref} className={`w-72 h-[100vh] sidecart absolute top-0 right-0 bg-pink-300 py-10 px-8 transform transition-transform ${Object.keys(cart).length!==0?'translate-x-0':'translate-x-full'}`}>
+      <div ref={ref} className={`w-72 h-[100vh] sidecart absolute top-0 right-0 bg-pink-300 py-10 px-8 transform transition-transform ${Object.keys(cart).length !== 0 ? 'translate-x-0' : 'translate-x-full'}`}>
         <h2 className="font-bold text-xl text-center">My cart</h2>
-        <span className="absolute top-2 right-4 font-bold text-3xl cursor-pointer text-pink-700" onClick={toggleCart}><IoMdCloseCircleOutline /></span>
+        <span className="absolute top-4 right-4 font-bold text-3xl cursor-pointer text-pink-700" onClick={toggleCart}><IoMdCloseCircleOutline /></span>
         <ol className='list-decimal font-semibold'>
           {Object.keys(cart).length == 0 && <div className='my-4 font-normal'>No item in the cart</div>}
-          { Object.keys(cart).map((k)=>{return<li key={k}>
-            <div className="item flex space-x-2 my-5">
-              <div className='w2/3 font-semibold'>{cart[k].name}</div>
-              <div className='font-semibold flex justify-center items-center'><FaMinus onClick={()=>removeFromcart(k,1,cart[k].price, cart[k].name, cart[k].size, cart[k].varient)} className='mx-3 text-sm cursor-pointer  text-pink-700'/> {cart[k].qty} <FaPlus onClick={()=>addTocart(k,1,cart[k].price, cart[k].name, cart[k].size, cart[k].varient)} className='mx-3 text-sm cursor-pointer  text-pink-700' /></div>
-            </div>
-          </li>})}
+          {Object.keys(cart).map((k) => {
+            return <li key={k}>
+              <div className="item flex space-x-2 my-5">
+                <div className='w2/3 font-semibold'>{cart[k].name}</div>
+                <div className='font-semibold flex justify-center items-center'><FaMinus onClick={() => removeFromcart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].varient)} className='mx-3 text-sm cursor-pointer  text-pink-700' /> {cart[k].qty} <FaPlus onClick={() => addTocart(k, 1, cart[k].price, cart[k].name, cart[k].size, cart[k].varient)} className='mx-3 text-sm cursor-pointer  text-pink-700' /></div>
+              </div>
+            </li>
+          })}
           <div className='text-left flex font-bold'>Total amount: <span className='ml-8'>₹{subTotal}</span></div>
         </ol>
-        
+
         <div className="flex justify-between space-x-1">
 
-        <button onClick={clearcart} className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-lg"> Clear cart</button>
-        <Link href='/checkout'><button className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-lg"><IoBagCheckOutline className='m-1'/> Checkout</button></Link>
+          <button onClick={clearcart} className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-lg"> Clear cart</button>
+          <Link href='/checkout'><button className="flex mx-auto mt-16 text-white bg-pink-500 border-0 py-2 px-2 focus:outline-none hover:bg-pink-600 rounded text-lg"><IoBagCheckOutline className='m-1' /> Checkout</button></Link>
         </div>
       </div>
     </div>
